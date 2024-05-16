de075cea4d5544801d17bcf57fec5fc70e2b2d88
karkisarkar <prabin@nydoz.com>
2024-05-16 17:18:38 +0545

    Pitch Deck Added in Product page

--- wp-content/themes/gowithfund/wpcftemplate/woocommerce/basic/include/campaign-tab.php
+++ wp-content/themes/gowithfund/wpcftemplate/woocommerce/basic/include/campaign-tab.php
@@ -24,14 +24,14 @@ defined( 'ABSPATH' ) || exit;
  * @see wpcf_default_single_campaign_tabs()
  */
 $tabs = apply_filters( 'wpcf_default_single_campaign_tabs', array() );
-$tabs['pitch_deck'] = array(
+$tabs['pitch_deck_pdf'] = array(
     'title'     => esc_html__( 'Pitch Deck', 'your-textdomain' ),
     'callback'  => function() {
         global $product;
 
         $pdf_url = get_post_meta( $product->get_id(), 'product_pdf_url', true );
     
-        $pdf_url = get_field('pitch_deck');
+        $pdf_url = get_field('pitch_deck_pdf');
        
         if ( $pdf_url ) {
             echo '<a href="' . esc_url( $pdf_url ) . '" target="_blank">Download PDF</a>';
