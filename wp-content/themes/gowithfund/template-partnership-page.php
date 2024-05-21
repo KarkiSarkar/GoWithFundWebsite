903f906425ace20cf994976f57780fb03f0606ad
karkisarkar <prabin@nydoz.com>
2024-05-21 15:19:11 +0545

    partnership single page template

--- wp-content/themes/gowithfund/template-partnership-page.php
+++ wp-content/themes/gowithfund/template-partnership-page.php
@@ -4,6 +4,8 @@ Template Name: Partner Single Page
 */
 get_header();
 $form = get_field('form_shortcode');
+$description = the_field('description');
+$key_feature = the_field('key_feature');
 ?>
 <style>
     .page-title{
@@ -16,9 +18,12 @@ $form = get_field('form_shortcode');
                 <h1 class="page-title"><?php the_title(); ?></h1>
             </header>
             <!-- <div style="display: flex;"> -->
+                <?php if(!empty($description)){?>
                 <div class="page-content" style="">
                     <p><?php the_field('description');?></p>
                 </div>
+                <?php  } ?>
+
                 <div style="">
                 <?php
                     if ( has_post_thumbnail() ) {
@@ -28,14 +33,18 @@ $form = get_field('form_shortcode');
 
                 </div>
             <!-- </div> -->
+            <?php if(!empty($key_feature)){?>
             <div class="page-content" style="padding-right: 10px;">
             <h4>Key Feature</h4>
                 <p><?php the_field('key_feature');?></p>
             </div>
+            <?php  } ?>
+            <?php if(!empty($form)){?>
             <div style="background: #00A9A5; width: 60%; padding-top: 3rem; margin: auto;">
                 <?php echo do_shortcode($form); ?>
             </div>
-            
+            <?php  } ?>
+
         </section>
     </main>
 
