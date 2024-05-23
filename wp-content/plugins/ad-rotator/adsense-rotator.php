bcd15bc183108dcb4d7a97f0341a453ca2ec945a
karkisarkar <prabin@nydoz.com>
2024-05-23 11:03:59 +0545

    adsense implemented

--- wp-content/plugins/ad-rotator/adsense-rotator.php
+++ wp-content/plugins/ad-rotator/adsense-rotator.php
@@ -84,10 +84,10 @@ function insert_ads_after_paragraph($content) {
 add_filter('the_content', 'insert_ads_after_paragraph');
 
 // Function to insert the AdSense ad shortcode into the header
-// function insert_ads_in_header() {
-//     echo do_shortcode('[rotate_named_adsense_ads]');
-// }
-// add_action('wp_head', 'insert_ads_in_header');
+function insert_ads_in_header() {
+    echo '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=client=' . $client_id . '" crossorigin="anonymous"></script>';
+}
+add_action('wp_head', 'insert_ads_in_header');
 
 // Shortcode to insert AdSense ad unit
 function rotate_named_adsense_ads_shortcode() {
