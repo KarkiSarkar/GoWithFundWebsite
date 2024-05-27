2659455b6700ed9a9c459214592de250d142b1e2
karkisarkar <prabin@nydoz.com>
2024-05-27 17:05:26 +0545

    partnertype form plugin added

--- wp-content/themes/gowithfund/template-partnership-page.php
+++ wp-content/themes/gowithfund/template-partnership-page.php
@@ -51,7 +51,7 @@ $key_feature = get_field('key_feature');
             </div>
             <?php  } ?>
             <?php if(!empty($form)){?>
-            <div style="background: #00A9A5; width: 60%; padding: 3rem; margin: auto;">
+            <div style="background: #00A9A5; width: 60%; padding: 3rem; margin: auto; ">
                 <?php echo do_shortcode($form); ?>
             </div>
             <?php  } ?>
